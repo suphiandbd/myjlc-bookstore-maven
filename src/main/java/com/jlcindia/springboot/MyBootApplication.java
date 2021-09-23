53fa822d3eb29d0ea2b135c4a15e9461dd54d4db
suphiandbd <89073515+suphiandbd@users.noreply.github.com>
2021-09-23 17:55:02 +0530

    Updated 3 for CI pipeline

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 System.out.println("Starting Boot Application");
 System.out.println("Updated 1 By SufyanKhan Today");
   System.out.println("Updated 2 By SufyanKhan Today");
+  System.out.println("Updated 3 for CI pipeline");
   SpringApplication.run(MyBootApplication.class, args);
 }
 }
