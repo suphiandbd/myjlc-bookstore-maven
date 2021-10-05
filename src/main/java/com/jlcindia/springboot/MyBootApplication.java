a91173bd23629bbb7fcfb8df4467c65b20432720
suphiandbd <89073515+suphiandbd@users.noreply.github.com>
2021-10-05 14:35:54 +0530

    Updating Boot Application 11

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 System.out.println("Starting Boot Application");
   System.out.println("Updating Boot Application");
   System.out.println("Updating Boot Application 10");
+  System.out.println("Updating Boot Application 11");
   SpringApplication.run(MyBootApplication.class, args);
 }
 }
