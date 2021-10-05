5c46c3536115fdeb65ff2a4d94c6bac6097c876d
suphiandbd <89073515+suphiandbd@users.noreply.github.com>
2021-10-05 14:25:38 +0530

    Updating Boot Application

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyBootApplication {
 public static void main(String[] args) {
 System.out.println("Starting Boot Application");
+  System.out.println("Updating Boot Application");
   SpringApplication.run(MyBootApplication.class, args);
 }
 }
